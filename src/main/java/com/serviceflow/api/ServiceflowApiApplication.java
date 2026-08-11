0572a35735302bce90881d2aed8cba92a2d0a46a
lgomesroc <lgomesroc2012@gmail.com>
2026-08-11 14:06:50 -0300

    chore: configure PostgreSQL connection

---
+++ src/main/java/com/serviceflow/api/ServiceflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.serviceflow.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceflowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceflowApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/serviceflow/api/ServiceflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviceflow.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceflowApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
